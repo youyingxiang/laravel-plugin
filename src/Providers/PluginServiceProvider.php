4370b0c472421d5390074efbcecbd87822823e7b
youyingxiang <1365831278@qq.com>
2021-12-10 14:31:00 +0800

    first commit

---
+++ src/Providers/PluginServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+namespace Yxx\LaravelPlugin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PluginServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+}
